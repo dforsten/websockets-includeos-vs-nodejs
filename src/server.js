d17a4d2216d70581cbba2ea68cd339c9f6a56963
davidf <dforsten@gmail.com>
2017-10-07 20:38:25 +0200

    Implemented a constant stream of send and reply between server and benchmarking script, need to batch connections over time, else they are refused by the server.+

--- src/server.js
+++ src/server.js
@@ -3,9 +3,9 @@ import WebSocket from 'ws';
 const wss = new WebSocket.Server({ port: 8080 });
 
 wss.on('connection', (ws) => {
-  console.log('client connected!');
+//  console.log('client connected!');
   ws.on('message', (message) => {
-    console.log(`Message received: ${message}`);
+    // console.log(`Message received: ${message}`);
     const json = JSON.parse(message);
     const reply = {
       type: 'move confirmed',
@@ -16,5 +16,11 @@ wss.on('connection', (ws) => {
 
   ws.send('{"something": 1}');
 });
+
+const printMetrics = () => {
+  console.log(`Number of connected clients: ${wss.clients.size}`);
+};
+
+setInterval(printMetrics, 5000);
 
 console.log('Started ws Server on localhost:8080');
--- src/worker.js
+++ src/worker.js
@@ -1,4 +1,23 @@
+import WebSocket from 'ws';
+
+const MAX_CONNECTIONS = 100;
+
+for (let i = 0; i < MAX_CONNECTIONS; ++i) {
+  const ws = new WebSocket('ws://localhost:8080');
+
+  ws.on('open', () => {
+    // console.log('Connected to ws server!');
+    ws.send('{"type": "initial state"}');
+  });
+
+  ws.on('message', () => {
+    // console.log(data);
+    ws.send('{"type": "initial state"}');
+  });
+}
+
 console.log('Worker started!');
+
 
 // process.on('message', (msg) => {
 //   console.log('Message from Controller:', msg);
